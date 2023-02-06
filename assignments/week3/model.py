51528ac6833d7d2efe5b0902f613bbb150868499
miaomiao612 <dddoctorr612@gmail.com>
2023-02-07 04:14:10 +0800

    update

--- assignments/week3/model.py
+++ assignments/week3/model.py
@@ -1,11 +1,12 @@
 """
 Author: miaomiao612 dddoctorr612@gmail.com
-Date: 2023-02-01 03:57:53
+Date: 2023-02-07 02:51:45
 LastEditors: miaomiao612 dddoctorr612@gmail.com
-LastEditTime: 2023-02-06 12:39:30
+LastEditTime: 2023-02-07 04:13:20
 FilePath: \week3\model.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 """
+
 from typing import Callable
 import torch
 
