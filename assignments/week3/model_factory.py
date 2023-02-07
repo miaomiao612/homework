9318bfdcedd7ebb2fcbad5d2673db9924ae9ad23
miaomiao612 <dddoctorr612@gmail.com>
2023-02-07 12:56:54 -0500

    update

--- assignments/week3/model_factory.py
+++ assignments/week3/model_factory.py
@@ -1,3 +1,11 @@
+'''
+Author: miaomiao612 dddoctorr612@gmail.com
+Date: 2023-02-06 13:51:45
+LastEditors: miaomiao612 dddoctorr612@gmail.com
+LastEditTime: 2023-02-07 12:56:22
+FilePath: \week3\model_factory.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 import torch
 from model import MLP
 
@@ -19,7 +27,7 @@ def create_model(input_dim: int, output_dim: int) -> MLP:
         input_dim,
         200,
         output_dim,
-        3,
+        2,
         torch.nn.ReLU,
         torch.nn.init.kaiming_uniform_,
     )
