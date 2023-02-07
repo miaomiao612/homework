c6bccc0dd4e97954b5ffa622bba9b7d27afc2e9e
miaomiao612 <dddoctorr612@gmail.com>
2023-02-07 13:31:52 -0500

    .

--- assignments/week3/model_factory.py
+++ assignments/week3/model_factory.py
@@ -1,3 +1,11 @@
+'''
+Author: miaomiao612 dddoctorr612@gmail.com
+Date: 2023-02-06 13:51:45
+LastEditors: miaomiao612 dddoctorr612@gmail.com
+LastEditTime: 2023-02-07 13:24:23
+FilePath: \week3\model_factory.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
 """
 Author: miaomiao612 dddoctorr612@gmail.com
 Date: 2023-02-06 13:51:45
@@ -27,7 +35,7 @@ def create_model(input_dim: int, output_dim: int) -> MLP:
         input_dim,
         200,
         output_dim,
-        2,
-        torch.nn.ReLU,
+        1,
+        torch.nn.LeakyReLU,
         torch.nn.init.kaiming_uniform_,
     )
