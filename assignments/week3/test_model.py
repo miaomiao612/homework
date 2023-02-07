771200e3fe425de52e5bb84957eb6544b63682a1
miaomiao612 <dddoctorr612@gmail.com>
2023-02-07 11:44:49 -0500

    black

--- assignments/week3/test_model.py
+++ assignments/week3/test_model.py
@@ -1,11 +1,3 @@
-'''
-Author: miaomiao612 dddoctorr612@gmail.com
-Date: 2023-02-07 02:51:45
-LastEditors: miaomiao612 dddoctorr612@gmail.com
-LastEditTime: 2023-02-08 00:25:52
-FilePath: \week3\test_model.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
 import inspect
 from model import MLP
 
